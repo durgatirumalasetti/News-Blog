ae9af403a20672deeac6487deb185a5499b7fadf
durgatirumalasetti <durgatirumalasetti306@gmail.com>
2026-03-17 14:44:24 +0530

    fix: use public folder for logo in Header.jsx for Vercel

--- src/components/Common/Header/Header.jsx
+++ src/components/Common/Header/Header.jsx
@@ -1,16 +1,11 @@
 import React from "react";
 import "./Header.css";
-import logo from "../../../assets/images/newsblog-logo.png";
 
 const Header = () => {
   return (
     <header className="header">
       <div className="logo-container">
-        <img
-          src={logo}
-          alt="NewsBlog Logo"
-          className="logo"
-        />
+        <img src="/newsblog-logo.png" alt="NewsBlog Logo" className="logo" />
       </div>
     </header>
   );
